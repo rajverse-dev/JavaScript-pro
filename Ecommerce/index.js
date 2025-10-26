acf652f1f7c579dbb9eec4dcfc69ed80272c319e
Raj-101206 <rrajasekar101206@gmail.com>
2025-10-26 17:34:43 +0530

    Add new project for Ecommerce

---
+++ Ecommerce/index.js
@@ -0,0 +1,7 @@
+let errorparagraph=document.getElementById("error");
+console.log(errorparagraph)
+
+function purchase(){
+    console.log("Button clicked");
+    errorparagraph.textContent="Something went wrong, please try again"
+}
--- exercise/practice.js
+++ exercise/practice.js
@@ -1,10 +1,18 @@
-let message="You have three";
-let neww="messages";
-console.log(message + "," + neww + "!" );
+// let message="You have three";
+// let neww="messages";
+// console.log(message + "," + neww + "!" );
 
 
-let welcomeEl=document.getElementById("welcome-el")
-let greeting="Welcome Back "
-let name="Raj..."
+// let welcomeEl=document.getElementById("welcome-el")
+// let greeting="Welcome Back "
+// let name="Raj..."
 
-welcomeEl.innerText=greeting+name;
+// welcomeEl.innerText=greeting+name;
+var firstName="Rajasekar";
+var lastName="R";
+var fullName=firstName+" " +lastName;
+console.log(fullName);
+
+
+
+
