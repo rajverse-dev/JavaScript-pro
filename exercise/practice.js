a12e137739443f7e993c80f0d0befa632d713bbe
Raj-101206 <rrajasekar101206@gmail.com>
2025-12-15 23:12:27 +0530

    some exercise

--- exercise/practice.js
+++ exercise/practice.js
@@ -1,18 +1,70 @@
-// let message="You have three";
-// let neww="messages";
-// console.log(message + "," + neww + "!" );
+// // let message="You have three";
+// // let neww="messages";
+// // console.log(message + "," + neww + "!" );
 
 
-// let welcomeEl=document.getElementById("welcome-el")
-// let greeting="Welcome Back "
-// let name="Raj..."
+// // let welcomeEl=document.getElementById("welcome-el")
+// // let greeting="Welcome Back "
+// // let name="Raj..."
 
-// welcomeEl.innerText=greeting+name;
-var firstName="Rajasekar";
-var lastName="R";
-var fullName=firstName+" " +lastName;
-console.log(fullName);
+// // welcomeEl.innerText=greeting+name;
+// //var firstName="Rajasekar";
+// // var lastName="R";
+// // var fullName=firstName+" " +lastName;
+// // console.log(fullName);
 
+// // var price=8988;
+// // var product="phone";
+// // var tax=56;
+// // console.log(product);
+// // var totalprice=price+tax;
+// // console.log(totalprice);
+// // ++tax;
+// // console.log(tax);
+// // console.log(typeof(tax));
+
+
+// ///////****FUNCTIONS */
+// iphone()
+// function iphone(){
+//     console.log("This is iphone");
+// }
+// iphone()
+// var a=20
+// var b=40
+// function add(){
+//     console.log(a+b)
+// }
+// add()
+// //exercise
+// var factor="Vijay"
+// var fplayer="Dhoni"
+// var fmovie="M.S.Dhoni"
+// function favourite(){
+//     console.log("Favourite Actor:",factor)
+//     console.log("Favourite Player:",fplayer)
+//     console.log("Favourite Movie:",fmovie)
+// }
+// favourite()
+
+// // next exercise
+// // function area(l,b){
+// //     console.log(l*b);
+// // }
+// // area(39,33)
+// //next
+// function area(l,b){
+//     return l*b
+// }
+// var res=area(44,10)
+// console.log(res)
+
+// //loo[p concept ]
+// for(i=5;i>=0;i--){
+//     var a="*2="
+//     console.log(i,a,i*2)
+// }
+//DOM MANIPULATION
 
 
 
